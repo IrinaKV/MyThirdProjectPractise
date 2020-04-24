a0841c3b373fe916e1c1a8e6c27f19e061c01b38
Irina Kiseleva <irina.kiseleva@icloud.com>
2020-04-24 16:59:15 -0400

    new

--- src/day01_intro/Intro.java
+++ src/day01_intro/Intro.java
@@ -3,6 +3,6 @@ package day01_intro;
 public class Intro {
     public static void main(String[] args) {
         System.out.println("New Pablish");
-        // I want to add here somthing
+        // I want to add here something
     }
 }
